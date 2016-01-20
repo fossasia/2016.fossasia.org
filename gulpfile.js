a0796a65c51f8819249b951471720c0fd9a8c6ae
Mohit Kanwal <mohit.kanwal@gmail.com>
2016-01-20 17:14:08 +0800

    Add pngquant and minify even more

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
 
 gulp.task('images-opt', function () {
     gulp.src('img/*.*')
-        .pipe(imagemin())
+        .pipe(imagemin({
+          progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
         .pipe(gulp.dest('img'));
 });
 
